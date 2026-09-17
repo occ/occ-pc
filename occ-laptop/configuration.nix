de749844e77f395341556726f6925d19669f7c1d
Onur Cakmak <occ@occ.me>
2026-09-17 16:48:08 -0400

    occ-laptop: re-add Flathub remote declaratively via systemd oneshot

--- occ-laptop/configuration.nix
+++ occ-laptop/configuration.nix
@@ -79,6 +79,20 @@
       };
       sops-install-secrets = waitForTpm;
       sops-install-secrets-for-users = waitForTpm;
+      # Flatpak state (incl. remotes) lives in /var/lib/flatpak and is wiped by
+      # a reinstall. Re-add Flathub so GNOME Software's catalog survives.
+      flatpak-add-flathub = {
+        description = "Add Flathub remote for Flatpak";
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        wantedBy = [ "multi-user.target" ];
+        path = [ pkgs.flatpak ];
+        serviceConfig.Type = "oneshot";
+        script = ''
+          flatpak remote-add --if-not-exists flathub \
+            https://dl.flathub.org/repo/flathub.flatpakrepo
+        '';
+      };
     };
 
   services = {
